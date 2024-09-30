d916e478f6ec80f25c86434d542a53a879c63b51
Maphikza <stapisi@protonmail.com>
2024-09-30 15:10:06 +0200

    Adding types file to ipc folder

--- internal/ipc/ipc.go
+++ internal/ipc/ipc.go
@@ -6,29 +6,9 @@ import (
 	"io"
 	"net"
 	"os"
-	"sync"
 )
 
 const socketPath = "/tmp/btc-wallet.sock"
-
-type Command struct {
-	ID      int      `json:"id"`
-	Command string   `json:"command"`
-	Args    []string `json:"args"`
-}
-
-type Response struct {
-	ID     int         `json:"id"`
-	Error  error       `json:"error,omitempty"`
-	Result interface{} `json:"result,omitempty"`
-}
-
-type Server struct {
-	listener    net.Listener
-	commands    chan Command
-	mutex       sync.Mutex
-	connections map[int]net.Conn // Maps command ID to the client connection
-}
 
 var commandID int
 
@@ -126,10 +106,6 @@ func (s *Server) SendResponse(id int, response Response) {
 
 func (s *Server) Close() error {
 	return s.listener.Close()
-}
-
-type Client struct {
-	conn net.Conn
 }
 
 func NewClient() (*Client, error) {
